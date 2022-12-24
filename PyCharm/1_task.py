cccb87c798932de866e07081d9c2767a2f727785
student-09-525 <student-09-525@ncfu.net>
2022-12-24 11:29:34 +0300

    test1

--- PyCharm/1_task.py
+++ PyCharm/1_task.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def positive():
-    print("Число положительное")
+import timeit
+from functools import lru_cache
 
-def negative():
-    print("Число отрицательное")
+def factorial_iteration(n):
+    product = 1
+    while n > 1:
+        product *= n
+        n -= 1
+    return product
 
-def test():
-    num = int(input("Введите число: "))
-    if num > 0:
-        positive()
-    elif num < 0:
-        negative()
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
     else:
-        print("Число равно нулю")
+        return n * factorial(n - 1)
 
 
-if __name__ == "__main__":
-    test()
+@lru_cache
+def factorial_cache(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial_cache(n - 1)
+print("Время обычной версии:")
+print(f'{timeit.timeit(lambda: factorial_iteration(200), number=10000)}, \n')
+print("Время рекурс. версии:")
+print(f'{timeit.timeit(lambda: factorial(200), number=10000)}, \n')
+print("Время рекурс. кеш. версии:")
+print(f'{timeit.timeit(lambda: factorial_cache(200), number=10000)}, \n')
